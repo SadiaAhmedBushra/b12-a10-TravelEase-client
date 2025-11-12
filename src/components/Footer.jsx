2ab4e85159e6124a450195a0bf8274d01b6d3855
Sadia Ahmed Bushra <sadiahmedbushra28@gmail.com>
2025-11-13 05:42:47 +0600

    commit 10: footer design updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
   return (
     <footer className="relative w-11/12 mx-auto mt-60 bg-gradient text-white pt-40 pb-10">
       <div
-        className="flex flex-col items-center gap-3 absolute -right-2/12 w-1/5 bg-opacity-10 rounded-full py-8 shadow-lg border-2 border-primary"
+        className="flex flex-col items-center gap-3 border-2 border-t-primary border-b-0 border-l-0 border-r-0 absolute left-1/2 w-2/3 max-w-4xl  bg-opacity-10 rounded-b-4xl py-10 shadow-lg"
         style={{ top: "-5rem", transform: "translateX(-50%)" }}
       >
         <div className="flex flex-row justify-between items-center gap-2">
@@ -23,6 +23,7 @@ const Footer = () => {
             <span className="font-bold text-[#6C757D] text-2xl">Ease</span>
           </NavLink>
         </div>
+    
         <Link to="/auth/register" className="btn-gradient font-bold">
           Register Now!
         </Link>
@@ -31,7 +32,7 @@ const Footer = () => {
       <div className="w-11/12 mx-auto flex flex-col justify-between items-center gap-5">
         <div className="flex flex-col lg:flex-row justify-between items-center gap-6">
           <div className="lg:w-2/3 flex flex-col items-center lg:items-start gap-3">
-            <h1 className="text-3xl">About Us</h1>
+            <h2 className="text-3xl mt-25 md:mt-25">About Us</h2>
             <p className="text-justify">
               TravelEase is your reliable companion for seamless vehicle booking
               and trip management. We empower travelers and vehicle owners alike
@@ -75,7 +76,7 @@ const Footer = () => {
           </nav>
         </div>
 
-        <div className="flex justify-between items-center gap-3 w-1/2 mx-auto text-sm">
+        <div className="flex justify-between items-center gap-3 w-1/2 text-sm">
           <p className="link link-hover">Privacy Policy</p>
           <p className="link link-hover">Cookies Policy</p>
           <p className="link link-hover">Terms and Conditions</p>
